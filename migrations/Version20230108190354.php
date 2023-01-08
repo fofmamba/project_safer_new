f43cb6730e630eeefb49870b5f42e1e97429e767
Kyria853 <kagbodouamen@univ-rennes1.fr>
2023-01-08 20:15:17 +0100

    final

--- migrations/Version20230108190354.php
+++ migrations/Version20230108190354.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230108165804 extends AbstractMigration
+final class Version20230108190354 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,7 +21,7 @@ final class Version20230108165804 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE administrateur CHANGE nom nom VARCHAR(255) DEFAULT NULL, CHANGE prenom prenom VARCHAR(255) DEFAULT NULL, CHANGE mail mail VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE bien ADD departement VARCHAR(255) NOT NULL, CHANGE descriptif descriptif VARCHAR(255) DEFAULT NULL, CHANGE image image VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE bien ADD surface VARCHAR(255) NOT NULL, ADD departement VARCHAR(255) NOT NULL, CHANGE descriptif descriptif VARCHAR(255) DEFAULT NULL, CHANGE image image VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE favoris CHANGE numero numero VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE utilisateur CHANGE roles roles JSON NOT NULL');
         $this->addSql('ALTER TABLE messenger_messages CHANGE delivered_at delivered_at DATETIME DEFAULT NULL');
@@ -31,7 +31,7 @@ final class Version20230108165804 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE administrateur CHANGE nom nom VARCHAR(255) DEFAULT \'NULL\', CHANGE prenom prenom VARCHAR(255) DEFAULT \'NULL\', CHANGE mail mail VARCHAR(255) DEFAULT \'NULL\'');
-        $this->addSql('ALTER TABLE bien DROP departement, CHANGE descriptif descriptif VARCHAR(255) DEFAULT \'NULL\', CHANGE image image VARCHAR(255) DEFAULT \'NULL\'');
+        $this->addSql('ALTER TABLE bien DROP surface, DROP departement, CHANGE descriptif descriptif VARCHAR(255) DEFAULT \'NULL\', CHANGE image image VARCHAR(255) DEFAULT \'NULL\'');
         $this->addSql('ALTER TABLE favoris CHANGE numero numero VARCHAR(255) DEFAULT \'NULL\'');
         $this->addSql('ALTER TABLE messenger_messages CHANGE delivered_at delivered_at DATETIME DEFAULT \'NULL\'');
         $this->addSql('ALTER TABLE utilisateur CHANGE roles roles LONGTEXT NOT NULL COLLATE `utf8mb4_bin`');
